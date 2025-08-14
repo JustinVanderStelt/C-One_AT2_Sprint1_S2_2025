e33ab14f1d55bce7b909b63e6f399cd223bbab91
justi <justi@JUSTINSHP>
2025-08-14 11:42:52 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace C_One_AT2_Sprint1_S2_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxOuterFOR = new System.Windows.Forms.TextBox();
+            this.textBoxInnerFOR = new System.Windows.Forms.TextBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.Location = new System.Drawing.Point(12, 27);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(271, 355);
+            this.listBoxResults.TabIndex = 0;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(403, 89);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(403, 167);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxOuterFOR
+            // 
+            this.textBoxOuterFOR.Location = new System.Drawing.Point(415, 238);
+            this.textBoxOuterFOR.Name = "textBoxOuterFOR";
+            this.textBoxOuterFOR.ReadOnly = true;
+            this.textBoxOuterFOR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOuterFOR.TabIndex = 3;
+            // 
+            // textBoxInnerFOR
+            // 
+            this.textBoxInnerFOR.Location = new System.Drawing.Point(415, 286);
+            this.textBoxInnerFOR.Name = "textBoxInnerFOR";
+            this.textBoxInnerFOR.ReadOnly = true;
+            this.textBoxInnerFOR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInnerFOR.TabIndex = 4;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(403, 129);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 5;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxInnerFOR);
+            this.Controls.Add(this.textBoxOuterFOR);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.listBoxResults);
+            this.Name = "Form1";
+            this.Text = "Astronomical processing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxOuterFOR;
+        private System.Windows.Forms.TextBox textBoxInnerFOR;
+        private System.Windows.Forms.TextBox textBoxSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_One_AT2_Sprint1_S2_2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Array of random integers
+        const int max = 24;
+        int[] myArray = new int[max];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myArray = new int[max];
+        }
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            for (int outer = 0; outer < max - 1; outer++)
+            {
+                for (int inner = 0; inner < max - 1; inner++)
+                {
+                    if (myArray[inner] > myArray[inner + 1])
+                    {
+                        // Swap routine
+                        temp = myArray[inner + 1];
+                        myArray[inner + 1] = myArray[inner];
+                        myArray[inner] = temp;
+                    }
+                    // Code to demostare the bubble sort 
+                    ShowArray();
+                    Application.DoEvents();
+                    Thread.Sleep(100);
+                    textBoxInnerFOR.Text = inner.ToString();
+                    textBoxOuterFOR.Text = outer.ToString();
+                }
+            }
+        }
+        // Method to deisplay array
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int mid;
+            int lowBound = 0;
+            int highBound = max;
+            int target;
+            if (!(Int32.TryParse(textBoxSearch.Text, out target)))
+            {
+                MessageBox.Show("You must eneter an integer");
+                return;
+            }
+            while (lowBound <= highBound) // Check "<" or "<="
+            {
+                // Display list
+                ShowArray();
+                // Find the mid-point
+                mid = (lowBound + highBound) / 2;
+                // Pause with a messagebox
+                MessageBox.Show("Low:" + lowBound + "Mid:" + mid + "High:" + highBound);
+                if (myArray[mid] == target)
+                {
+                    // Target has been found
+                    listBoxResults.Items.Add("Found at index " + mid);
+                    return;
+                }
+                else if (myArray[mid] >= target)
+                {
+                    highBound = mid - 1;
+                }
+                else
+                {
+                    lowBound = mid + 1;
+                }
+                MessageBox.Show("Not Found, try again.");
+
+            }
+        }
+        //Mehtod to display array    
+        private void ShowArray()
+        {
+            listBoxResults.Items.Clear();
+            for (int i = 0; i < max; i++)
+            {
+                listBoxResults.Items.Add(myArray[i]);
+            }
+        }
+        // Method to fill Array with random numbers
+
+        private void FillArray()
+        {
+            // Create a random number
+            Random rand = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                // Random number 10..90
+                myArray[i] = rand.Next(10, 90);
+            }
+        }
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
